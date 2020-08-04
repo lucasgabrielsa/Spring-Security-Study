bfe7df91308a87c79e6d446ea9e427feea7cfe8d
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-08-04 16:30:46 -0300

    Progress on Course Development

--- src/main/java/com/study/demo/controllers/StudentManagementController.java
+++ src/main/java/com/study/demo/controllers/StudentManagementController.java
@@ -1,6 +1,8 @@
 package com.study.demo.controllers;
 
 import com.study.demo.models.Student;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -8,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("management/api/v1/students")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class StudentManagementController {
 
     private static List<Student> STUDENTS_LIST =
@@ -15,22 +18,28 @@ public class StudentManagementController {
                     new Student(2, "Jone Doe"),
                     new Student(3, "James Dean"));
 
+
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ADMIN_TRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS_LIST;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
        System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         student.setStudentId(studentId);
         System.out.println(String.format("%s %s", studentId, student));
--- src/main/java/com/study/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/study/demo/security/ApplicationSecurityConfig.java
@@ -30,14 +30,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+//                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
